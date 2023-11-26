8d6877ed9d3baeac67dc5e47d60b962f91dbf66c
Nobody <nobody@nowhere.net>
2023-11-25 20:45:50 -0800

    shared vpc fix

--- modules/vpc-networks/outputs.tf
+++ modules/vpc-networks/outputs.tf
@@ -1,9 +1,17 @@
 output "vpc_networks" {
   description = "VPC Networks"
-  value = { for k, v in local.vpc_networks :
-    v.key => {
-      name = try(google_compute_network.default[v.key].name, null)
-      id   = try(google_compute_network.default[v.key].id, null)
-    } if v.create
+  value = { for n in local.vpc_networks :
+    n.key => {
+      name      = try(google_compute_network.default[n.key].name, null)
+      id        = try(google_compute_network.default[n.key].id, null)
+      self_link = try(google_compute_network.default[n.key].self_link, null)
+      subnets = [for i, s in local.subnets :
+        {
+          name     = s.name
+          region   = s.region
+          ip_range = s.ip_range
+          id       = try(google_compute_subnetwork.default[s.key].id, null)
+      } if s.network == n.name]
+    } if n.create
   }
 }
