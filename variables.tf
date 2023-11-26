230ece69db4b80f211b365c19b009509618701f6
Nobody <nobody@nowhere.net>
2023-11-25 22:43:58 -0800

    new module

--- variables.tf
+++ variables.tf
@@ -214,6 +214,37 @@ variable "service_attachments" {
   }))
   default = []
 }
+variable "instances" {
+  type = list(object({
+    create                   = optional(bool, true)
+    project_id               = optional(string)
+    name                     = optional(string)
+    description              = optional(string)
+    region                   = optional(string)
+    zone                     = optional(string)
+    network_name             = optional(string)
+    subnet_name              = optional(string)
+    machine_type             = optional(string)
+    boot_disk_type           = optional(string)
+    boot_disk_size           = optional(number)
+    image                    = optional(string)
+    os                       = optional(string)
+    os_project               = optional(string)
+    startup_script           = optional(string)
+    service_account_email    = optional(string)
+    service_account_scopes   = optional(list(string))
+    network_tags             = optional(list(string))
+    enable_ip_forwarding     = optional(bool)
+    enable_delete_protection = optional(bool)
+    nat_ip_addresses         = optional(list(string))
+    nat_ip_names             = optional(list(string))
+    ssh_key                  = optional(string)
+    create_instance_groups   = optional(bool)
+    public_zone              = optional(string)
+    private_zone             = optional(string)
+  }))
+  default = []
+}
 variable "defaults" {
   type = object({
     cloud_router_bgp_asn                   = optional(number)
