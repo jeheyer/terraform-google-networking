ba344a9ad408742ee280e72f25e5bb61395b8024
Nobody <nobody@nowhere.net>
2023-11-27 11:40:21 -0800

    add outputs

--- modules/service-attachments/outputs.tf
+++ modules/service-attachments/outputs.tf
@@ -3,8 +3,10 @@ output "service_attachments" {
   description = "PSC Published Services"
   value = { for i, v in local.service_attachments :
     v.key => {
-      name      = try(google_compute_service_attachment.default[v.key].name, null)
-      self_link = try(google_compute_service_attachment.default[v.key].self_link, null)
+      name                = try(google_compute_service_attachment.default[v.key].name, null)
+      self_link           = try(google_compute_service_attachment.default[v.key].self_link, null)
+      self_link           = try(google_compute_service_attachment.default[v.key].self_link, null)
+      connected_endpoints = try(google_compute_service_attachment.default[v.key].connected_endpoints, null)
     } if v.create
   }
 }
