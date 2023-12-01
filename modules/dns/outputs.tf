7770f5ef10534655519257a30d7f999ad474812f
Nobody <nobody@nowhere.net>
2023-12-01 08:41:29 -0800

    outputs

--- modules/dns/outputs.tf
+++ modules/dns/outputs.tf
@@ -1,18 +1,20 @@
 
 output "dns_zones" {
-  value = { for i, v in local.dns_zones :
-    v.key => {
+  value = [for i, v in local.dns_zones :
+    {
+      key          = v.key
       name         = try(google_dns_managed_zone.default[v.key].name, null)
       dns_name     = try(google_dns_managed_zone.default[v.key].dns_name, null)
       name_servers = try(google_dns_managed_zone.default[v.key].name_servers, null)
       visibility   = v.visibility
-    } if v.create
-  }
+    }
+  ]
 }
 output "dns_policies" {
-  value = { for k, v in local.dns_policies :
-    v.key => {
+  value = [for i, v in local.dns_policies :
+    {
+      key  = v.key
       name = try(google_dns_policy.default[v.key].name, null)
-    } if v.create
-  }
+    }
+  ]
 }
--- modules/instances/outputs.tf
+++ modules/instances/outputs.tf
@@ -1,19 +1,21 @@
 output "instances" {
   description = "Instances"
-  value = { for i, v in local.instances :
-    v.key => {
+  value = [for i, v in local.instances :
+    {
+      key  = v.key
       name = try(google_compute_instance.default[v.key].name, null)
       zone = try(google_compute_instance.default[v.key].zone, null)
-    } if v.create
-  }
+    }
+  ]
 }
 
 output "umigs" {
   description = "Unmanaged Instance Groups"
-  value = { for i, v in local.umigs :
-    v.key => {
+  value = [for i, v in local.umigs :
+    {
+      key  = v.key
       name = try(google_compute_instance_group.default[v.key].name, null)
       zone = try(google_compute_instance_group.default[v.key].zone, null)
-    } if v.create
-  }
+    }
+  ]
 }
