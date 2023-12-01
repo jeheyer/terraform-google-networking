7bf45cf7cd63a86449a13ad33a31a8c73e3776ef
Nobody <nobody@nowhere.net>
2023-11-30 22:10:36 -0800

    output updates

--- modules/vpc-networks/firewall_rules.tf
+++ modules/vpc-networks/firewall_rules.tf
@@ -3,7 +3,7 @@ locals {
     [for vpc_network in local.vpc_networks :
       [for i, v in coalesce(vpc_network.firewall_rules, []) :
         merge(v, {
-          project_id = coalesce(v.project_id, var.project_id)
+          project_id   = coalesce(v.project_id, var.project_id)
           network_name = vpc_network.name
         })
       ]
@@ -13,6 +13,6 @@ locals {
 
 module "firewall-rules" {
   source         = "git::https://github.com/jeheyer/terraform-google-networking//modules/firewall"
-  project_id = var.project_id
+  project_id     = var.project_id
   firewall_rules = local.firewall_rules
 }
