1ada475099bcc5b8ce95af11bc7ba192ce041fde
Nobody <nobody@nowhere.net>
2023-11-30 22:03:59 -0800

    output updates

--- modules/vpc-networks/outputs.tf
+++ modules/vpc-networks/outputs.tf
@@ -1,17 +1,18 @@
 output "vpc_networks" {
   description = "VPC Networks"
-  value = { for n in local.vpc_networks :
-    n.key => {
-      name      = try(google_compute_network.default[n.key].name, null)
-      id        = try(google_compute_network.default[n.key].id, null)
-      self_link = try(google_compute_network.default[n.key].self_link, null)
+  value = [for i, v in local.vpc_networks :
+    {
+      key       = v.key
+      name      = try(google_compute_network.default[v.key].name, null)
+      id        = try(google_compute_network.default[v.key].id, null)
+      self_link = try(google_compute_network.default[v.key].self_link, null)
       subnets = [for i, s in local.subnets :
         {
           name     = s.name
           region   = s.region
           ip_range = s.ip_range
           id       = try(google_compute_subnetwork.default[s.key].id, null)
-      } if s.network == n.name]
-    } if n.create
-  }
+      } if s.network == v.name]
+    }
+  ]
 }
