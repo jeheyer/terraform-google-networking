0483ee6f731d39063c3424ca1707fd547b05c6e9
Nobody <nobody@nowhere.net>
2023-12-02 13:44:17 -0800

    new module

--- modules/healthchecks/outputs.tf
+++ modules/healthchecks/outputs.tf
@@ -3,12 +3,13 @@ output "healthchecks" {
     {
       name   = v.name
       region = v.region
-      id = one(concat(
-        v.is_regional && !v.is_legacy ? google_compute_region_health_check.default[v.key].id : [],
-        !v.is_regional && !v.is_legacy ? google_compute_health_check.default[v.key].id : [],
-        v.is_legacy && v.is_http ? google_compute_http_health_check.default[v.key].id : [],
-        v.is_legacy && v.is_http ? google_compute_https_health_check.default[v.key].id : [],
-      ))
+      id = coalesce(
+        v.is_regional && !v.is_legacy ? google_compute_region_health_check.default[v.key].id : null,
+        !v.is_regional && !v.is_legacy ? google_compute_health_check.default[v.key].id : null,
+        v.is_legacy && v.is_http ? google_compute_http_health_check.default[v.key].id : null,
+        v.is_legacy && v.is_http ? google_compute_https_health_check.default[v.key].id : null,
+        "unknown"
+      )
     }
   ]
 }
