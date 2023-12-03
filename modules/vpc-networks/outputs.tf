56eb343410ed1d590462243b046e11781e34e155
Nobody <nobody@nowhere.net>
2023-12-03 14:18:13 -0800

    bug fix

--- modules/vpc-networks/outputs.tf
+++ modules/vpc-networks/outputs.tf
@@ -1,28 +1,36 @@
 output "vpc_networks" {
   description = "VPC Networks"
-  value = [for i, v in local.vpc_networks :
+  value = [for vpc_network in local.vpc_networks :
     {
-      index_key = v.index_key
-      name      = try(google_compute_network.default[v.index_key].name, null)
-      id        = try(google_compute_network.default[v.index_key].id, null)
-      self_link = try(google_compute_network.default[v.index_key].self_link, null)
-      subnets = [for i, s in local.subnets :
+      index_key = vpc_network.index_key
+      name      = try(google_compute_network.default[vpc_network.index_key].name, null)
+      id        = try(google_compute_network.default[vpc_network.index_key].id, null)
+      self_link = try(google_compute_network.default[vpc_network.index_key].self_link, null)
+      subnets = [for subnet in local.subnets :
         {
-          name     = s.name
-          region   = s.region
-          ip_range = s.ip_range
-          id       = try(google_compute_subnetwork.default[s.index_key].id, null)
-      } if s.network == v.name]
-      peering_connections = [for i, p in local.peerings :
+          name     = subnet.name
+          region   = subnet.region
+          ip_range = subnet.ip_range
+          id       = try(google_compute_subnetwork.default[subnet.index_key].id, null)
+      } if subnet.network == vpc_network.name]
+      peering_connections = [for peering_connection in local.peerings :
         {
-          peer_network  = p.peer_network
-          state         = try(google_compute_network_peering.default[p.index_key].state, null)
-          state_details = try(google_compute_network_peering.default[p.index_key].state_details, null)
+          peer_network  = peering_connection.peer_network
+          state         = try(google_compute_network_peering.default[peering_connection.index_key].state, null)
+          state_details = try(google_compute_network_peering.default[peering_connection.index_key].state_details, null)
         }
       ]
-      cloud_routers = [for i, r in local.cloud_routers :
+      cloud_routers = [for cloud_router in local.cloud_routers :
         {
-          name = r.name
+          name = cloud_router.name
+          region = cloud_router.region
+        }
+      ]
+      cloud_nats = [for cloud_nat in local.cloud_nats :
+        {
+          name = cloud_nat.name
+          region = cloud_nat.region
+          router = cloud_nat.router
         }
       ]
     }
