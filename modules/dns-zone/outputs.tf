6940740368cd6ae51a7834d7f4ca19de81ec5f59
Nobody <nobody@nowhere.net>
2023-12-07 19:44:00 -0800

    cleanup

--- modules/dns-zone/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "name" { value = try(one(google_dns_managed_zone.default).name, null) }
-output "description" { value = try(one(google_dns_managed_zone.default).description, null) }
-output "dns_name" { value = try(one(google_dns_managed_zone.default).dns_name, null) }
-output "name_servers" { value = try(one(google_dns_managed_zone.default).name_servers, null) }
-output "visibility" { value = try(local.dns_zone.visibility, null) }
